016fa8d573d4d7310d03be8cf6261e030b27967c
jaewoo <9712jw@gmail.com>
2026-01-22 20:29:47 +0900

    feat : project init

---
+++ src/main/java/com/dnd/donghaeng/DonghaengApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.donghaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonghaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonghaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/donghaeng/DonghaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.donghaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonghaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
