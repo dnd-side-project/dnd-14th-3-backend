7eec33720f4239f353617b153ffa1170a8b5a5b4
jaewoo <9712jw@gmail.com>
2026-01-30 11:44:44 +0900

    chore : 프로젝트 이름으로 진입 클래스 이름 수정

--- src/main/java/com/dnd/jjigeojulge/JjigeojulgeApplicationTests.java
+++ src/main/java/com/dnd/jjigeojulge/JjigeojulgeApplicationTests.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DonghaengApplication {
+public class JjigeojulgeApplicationTests {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DonghaengApplication.class, args);
+		SpringApplication.run(JjigeojulgeApplicationTests.class, args);
 	}
 
 }
