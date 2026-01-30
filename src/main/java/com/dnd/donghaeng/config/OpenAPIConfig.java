82509f7d792e2fd19df8cb79e9c749a1370416fb
jaewoo <9712jw@gmail.com>
2026-01-30 22:00:43 +0900

    chore : api 문서 도구 스웨거 설정 추가
    # Conflicts:
    #	build.gradle
    #	src/main/resources/application.yaml

---
+++ src/main/java/com/dnd/donghaeng/config/OpenAPIConfig.java
@@ -0,0 +1,21 @@
+package com.dnd.donghaeng.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "찍어줄게 SWAGGER", description = "\uD83D\uDCF8 찍어줄게 API 명세서", version = "v1"))
+public class OpenAPIConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI().components(new Components().addSecuritySchemes("bearer-key",
+			new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+	}
+}
