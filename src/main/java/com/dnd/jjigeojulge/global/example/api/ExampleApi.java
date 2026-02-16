1d900544381c3d2693fc7d3507091a74084d7110
jihwan38 <sss203800@gmail.com>
2026-02-16 19:48:56 +0900

    refactor: 예제 코드 위치 변경 및 구조 재정비
    도메인 로직과 무관한 API 레퍼런스용 예제 코드(Example)를 presentation 계층에서 분리하여 global 패키지로 이동함

---
+++ src/main/java/com/dnd/jjigeojulge/global/example/api/ExampleApi.java
@@ -0,0 +1,25 @@
+package com.dnd.jjigeojulge.global.example.api;
+
+import org.springframework.http.ResponseEntity;
+
+import com.dnd.jjigeojulge.global.common.ApiResponse;
+import com.dnd.jjigeojulge.global.example.response.ExampleDtoRecord;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Example API", description = "Example API endpoints")
+public interface ExampleApi {
+
+	@Operation(summary = "예시 API 엔드포인트", description = "이 엔드포인트는 [예시] 응답을 반환합니다.")
+	@ApiResponses(
+		value = {
+			@io.swagger.v3.oas.annotations.responses.ApiResponse(
+				responseCode = "200",
+				description = "성공적으로 예시 응답을 반환함"
+			)}
+	)
+	ResponseEntity<ApiResponse<ExampleDtoRecord>> exampleEndpoint();
+
+}
