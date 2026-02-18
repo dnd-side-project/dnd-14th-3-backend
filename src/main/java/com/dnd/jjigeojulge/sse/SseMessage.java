ab5ec75f582224a2ad0b121c61c7c9100d02ce1d
jaewoo <9712jw@gmail.com>
2026-02-19 05:16:28 +0900

    feat: SSE Message 발행 이벤트 핸들러 - 이벤트 메시지 생성 정적 메서드 추가 - 이벤트 핸들러를 통해 sse 서비스로 send 요청

--- src/main/java/com/dnd/jjigeojulge/sse/SseMessage.java
+++ src/main/java/com/dnd/jjigeojulge/sse/SseMessage.java
@@ -1,5 +1,13 @@
 package com.dnd.jjigeojulge.sse;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +18,34 @@ import lombok.Setter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SseMessage {
 
+	private UUID eventId;
 	private String eventName;
 	private Object eventData;
+	private Set<Long> receiverIds = new HashSet<>();
+
+	public static SseMessage create(Long receiverId, String eventName, Object eventData) {
+		return new SseMessage(
+			UUID.randomUUID(),
+			eventName,
+			eventData,
+			Set.of(receiverId)
+		);
+	}
+
+	public static SseMessage create(Collection<Long> receiverIds, String eventName, Object eventData) {
+		return new SseMessage(
+			UUID.randomUUID(),
+			eventName,
+			eventData,
+			new HashSet<>(receiverIds)
+		);
+	}
+
+	public Set<ResponseBodyEmitter.DataWithMediaType> toEvent() {
+		return SseEmitter.event()
+			.id(eventId.toString())
+			.name(eventName)
+			.data(eventData)
+			.build();
+	}
 }
