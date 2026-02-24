88575e0a4825b4d8656d5d92143f8acaa381fe3a
jaewoo <9712jw@gmail.com>
2026-02-24 13:44:01 +0900

    refactor: swagger 헤더에 인증 코드 첨부 추가

--- src/main/java/com/dnd/jjigeojulge/config/OpenAPIConfig.java
+++ src/main/java/com/dnd/jjigeojulge/config/OpenAPIConfig.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
@@ -23,7 +24,12 @@ public class OpenAPIConfig {
 				new Server().url("http://localhost:8080").description("로컬 서버"),
 				new Server().url("https://api.snapforyou.cloud").description("프로덕션 서버")
 			))
-			.components(new Components().addSecuritySchemes("bearer-key",
-				new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+			.components(new Components()
+				.addSecuritySchemes("bearer-key",
+					new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")))
+			.addSecurityItem(new SecurityRequirement().addList("bearer-key"));
 	}
 }
