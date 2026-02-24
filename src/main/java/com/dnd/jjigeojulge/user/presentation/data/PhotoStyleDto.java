2383b2d84cc0ff0d824354cf549914a00666747c
jaewoo <9712jw@gmail.com>
2026-02-24 20:14:06 +0900

    feat: update PhotoStyleDto to use String for name and modify findAll method in PhotoStyleController

--- src/main/java/com/dnd/jjigeojulge/user/presentation/data/PhotoStyleDto.java
+++ src/main/java/com/dnd/jjigeojulge/user/presentation/data/PhotoStyleDto.java
@@ -1,9 +1,7 @@
 package com.dnd.jjigeojulge.user.presentation.data;
 
-import com.dnd.jjigeojulge.user.domain.StyleName;
-
 public record PhotoStyleDto(
 	Long id,
-	StyleName name
+	String name
 ) {
 }
